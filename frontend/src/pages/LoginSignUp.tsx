53f823f565da2a4f2420af8171739fb73cf614be
terenceyin <terence.yin76@gmail.com>
2025-01-18 13:52:01 -0800

    firebase auth 2

--- frontend/src/pages/LoginSignUp.tsx
+++ frontend/src/pages/LoginSignUp.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import LogInButton from "../auth/LogInButton";
+import { auth } from "../auth/firebaseConfig";
 
 function LoginSignUp() {
-  return <div>LOGIN SIGNUP</div>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
+      if (currentUser) {
+        const token = await currentUser.getIdToken();
+        localStorage.setItem("firebaseToken", token);
+        setUser({
+          name: currentUser.displayName,
+          email: currentUser.email,
+        });
+      } else {
+        localStorage.removeItem("firebaseToken");
+        setUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return <LogInButton setUser={setUser}></LogInButton>;
 }
 
 export default LoginSignUp;
