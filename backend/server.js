49bd3f6c11371f437d1d9cfe3f90538c811d86d0
xuanhieu2611 <hieulexuan261103@gmail.com>
2025-01-18 13:19:05 -0800

    backend init

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+// Cors
+app.use(cors())
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded())
+
+// Routes
+app("/", (req, res) => res.status(200).json("Backend is running!"))
+
+app.listen(5000, () => {
+  console.log("Server started at port 5000")
+})
