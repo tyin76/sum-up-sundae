6366b5da6de123503af0975e83a6adb3eccb119d
terenceyin <terence.yin76@gmail.com>
2025-01-18 12:59:45 -0800

    made pages and downloaded mui

---
+++ frontend/src/pages/CreateJoinGroup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateJoinGroup() {
+  return <div>CREATE JOIN GROUP</div>;
+}
+
+export default CreateJoinGroup;
---
+++ frontend/src/pages/LoginSignUp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginSignUp() {
+  return <div>LOGIN SIGNUP</div>;
+}
+
+export default LoginSignUp;
