417e2dd8237dfeb8361fd0a5bc4439ac884f9bcd
faster <faster@students.cs.ubc.ca>
2025-01-18 16:09:42 -0800

    feat: successfully connect to MongoDB
    - Established connection to MongoDB using Mongoose.
    - Loaded MONGO_URI from environment variables.
    - Verified connection with a log message on successful startup.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import { lp } from "./livepeer/livepeer.js"
+import connectDB from "./mongodb/mongo.js"
 
 dotenv.config()
 
@@ -17,7 +17,7 @@ app.use(express.urlencoded())
 // Routes
 app.get("/", (req, res) => res.status(200).json("Backend is running!"))
 
-app.listen(5000, () => {
-  lp()
+app.listen(5432, () => {
+  connectDB();
   console.log("Server started at port 5000")
 })
