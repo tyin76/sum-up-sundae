34386dc82862b4d77ac18d67ac6f005dcd488176
terenceyin <terence.yin76@gmail.com>
2025-01-19 03:51:19 -0800

    added leave group button

--- frontend/src/pages/CreateJoin.js
+++ frontend/src/pages/CreateJoin.js
@@ -1,6 +1,6 @@
 import CustomButton from "../components/CustomButton";
 import { useEffect, useState } from "react"
-import { Stack, Box } from "@mui/material"
+import { Stack, Box, Button, Typography } from "@mui/material"
 import { TextField } from "@mui/material"
 import { joinGroup, createGroup, getGroupId } from '../api/api'
 import { create } from "@mui/material/styles/createTransitions";
@@ -29,50 +29,79 @@ function CreateJoin() {
     }
 
     return (
-        <Stack width={"100%"} alignItems={'center'}>
-            {groupId ? <>
-                <Stack>
-                    <TextField
-                        placeholder="Enter group code"
-                        onChange={(e) => setGroupCode(e.target.value)}
-                        sx={{
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': {
-                                    borderColor: '#DABCDF',
-                                    transition: 'border-color 0.3s ease-in-out'
-                                },
-                                '&:hover fieldset': {
-                                    borderColor: '#FBB6BA',
-                                },
-                                '&.Mui-focused fieldset': {
-                                    borderColor: '#FBB6BA',
-                                },
-                            },
-                            '& .MuiInputBase-input::placeholder': {
-                                color: '#7D1945',
-                                fontFamily: 'Bubble',
-                                fontSize: '20px'
-                            },
-                            '& .MuiInputBase-input': {
-                                color: '#7D1945',
-                                fontFamily: 'Bubble',
-                                fontSize: '20px',
-                            }
-                        }}
-                        InputProps={{
-                            style: {
-                                borderTopRightRadius: '2em',
-                                borderTopLeftRadius: '2em'
-                            }
-                        }}
-                    ></TextField>
-                    <CustomButton width="100%" borderTopRightRadius="0" borderTopLeftRadius="0" borderBottomRightRadius="1em" borderBottomLeftRadius="1em"
-                        onClick={() => handleClick(groupCode)}>Join Group</CustomButton>
-                </Stack>
-                <Box sx={{ height: "30px" }} />
-                <CustomButton onClick={() => handleCreateGroupClick()}>Create Group</CustomButton></> : "remove"}
-        </Stack>
-    );
+        <>
+          {groupId ? (
+            <Stack width="100%" alignItems="center">
+              <Stack>
+                <TextField
+                  placeholder="Enter group code"
+                  onChange={(e) => setGroupCode(e.target.value)}
+                  sx={{
+                    '& .MuiOutlinedInput-root': {
+                      '& fieldset': {
+                        borderColor: '#DABCDF',
+                        transition: 'border-color 0.3s ease-in-out',
+                      },
+                      '&:hover fieldset': {
+                        borderColor: '#FBB6BA',
+                      },
+                      '&.Mui-focused fieldset': {
+                        borderColor: '#FBB6BA',
+                      },
+                    },
+                    '& .MuiInputBase-input::placeholder': {
+                      color: '#7D1945',
+                      fontFamily: 'Bubble',
+                      fontSize: '20px',
+                    },
+                    '& .MuiInputBase-input': {
+                      color: '#7D1945',
+                      fontFamily: 'Bubble',
+                      fontSize: '20px',
+                    },
+                  }}
+                  InputProps={{
+                    style: {
+                      borderTopRightRadius: '2em',
+                      borderTopLeftRadius: '2em',
+                    },
+                  }}
+                />
+                <CustomButton
+                  width="100%"
+                  borderTopRightRadius="0"
+                  borderTopLeftRadius="0"
+                  borderBottomRightRadius="1em"
+                  borderBottomLeftRadius="1em"
+                  onClick={() => handleClick(groupCode)}
+                >
+                  Join Group
+                </CustomButton>
+              </Stack>
+              <Box sx={{ height: "30px" }} />
+              <CustomButton onClick={() => handleCreateGroupClick()}>Create Group</CustomButton>
+              <CustomButton>Leave Current Group</CustomButton>
+            </Stack>
+          ) : (
+            <Box
+                sx={{
+                    display: 'flex', 
+                    flexDirection: 'column',             
+                    alignItems: 'center',        
+                    justifyContent: 'center',    
+                    height: '100vh',             
+                    width: '100vw',              
+                    backgroundColor: '#f5f5f5',
+                    }}
+                    >
+            <Typography sx={{ fontFamily: 'Bubble', paddingBottom: '40px', fontSize: '40px'}}>You are already in a Group, you cannot join more than one!</Typography>
+            <CustomButton>Leave Current Group</CustomButton>
+            </Box>
+
+          )}
+        </>
+      );
+      
 }
 
 export default CreateJoin;
