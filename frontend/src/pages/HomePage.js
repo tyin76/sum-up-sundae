593e87cbe9763991115deb9d722a87a3386f3b12
terenceyin <terence.yin76@gmail.com>
2025-01-18 16:34:37 -0800

    styled homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,40 +1,60 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import LoginButton from '../auth/LoginButton.js';
 import LogoutButton from '../auth/LogoutButton.js';
 import { auth } from '../auth/firebaseConfig';
+import { Box, Typography } from '@mui/material';
 
 function HomePage() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
-            if (currentUser) {
-                const token = await currentUser.getIdToken();
-                localStorage.setItem('firebaseToken', token);
-                setUser({
-                    name: currentUser.displayName,
-                    email: currentUser.email,
-                });
-            } else {
-                localStorage.removeItem('firebaseToken');
-                setUser(null);
-            }
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
+      if (currentUser) {
+        const token = await currentUser.getIdToken();
+        localStorage.setItem('firebaseToken', token);
+        setUser({
+          name: currentUser.displayName,
+          email: currentUser.email,
         });
-        return () => unsubscribe();
-    }, []);
+      } else {
+        localStorage.removeItem('firebaseToken');
+        setUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <div>
-        
-      {!user && <LoginButton setUser={setUser} /> }
-        {user && <h1>welcome {user.name}</h1> }
-        
+    <Box
+      display="flex"
+      height="100vh"
+    >
+      
+      <Box
+        flex={1}
+        bgcolor="#FBB6BA"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        {user && <Typography variant="h1">Welcome, {user.name}!</Typography>}
+      </Box>
 
+      
+      <Box
+        flex={1}
+        bgcolor="#FFF5F4"
+        display="flex"
+        flexDirection='column'
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography>Sum-up Sundae</Typography>
+        {!user && <LoginButton setUser={setUser} />}
         {user && <LogoutButton setUser={setUser} />}
-        
-    </div>
-    
-  )
+      </Box>
+    </Box>
+  );
 }
 
-export default HomePage
+export default HomePage;
