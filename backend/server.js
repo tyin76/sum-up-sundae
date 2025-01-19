9e31208910f54f8a9ec0927b6f2fa367c305ffee
xuanhieu2611 <hieulexuan261103@gmail.com>
2025-01-18 16:53:09 -0800

    livepeer init

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ app.use(express.urlencoded())
 // Routes
 app.get("/", (req, res) => res.status(200).json("Backend is running!"))
 
-app.listen(5000, () => {
+const server = () => {
   lp()
-  console.log("Server started at port 5000")
-})
+  app.listen(5000, () => {
+    console.log("Server started at port 5000")
+  })
+}
+
+server()
